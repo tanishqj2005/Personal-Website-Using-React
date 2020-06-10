95368307d39280ca6bcf7dc18436d16288cb6a3c
Tanishq Joshi <tanishquj@gmail.com>
2020-06-10 18:22:33 +0530

    Adding All React Based Files

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hi! Tanishq's Website is still under development.</h1>
+    </div>
+  );
+}
+
+export default App;
