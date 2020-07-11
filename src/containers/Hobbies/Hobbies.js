eccd7eef2af0bb633585397e88bc95f5c4a59992
Tanishq Joshi <tanishquj@gmail.com>
2020-07-11 12:15:02 +0530

    Making Hobbies Responsive

--- src/containers/Hobbies/Hobbies.js
+++ src/containers/Hobbies/Hobbies.js
@@ -1,5 +1,5 @@
-import React,{Component} from 'react';
-import classes from './Hobbies.module.css';
+import React, { Component } from "react";
+import classes from "./Hobbies.module.css";
 import HobbyInfo from "../../components/UI/Hobby/HobbyInfo";
 import Hobby3 from "../../components/UI/Hobby/Hobby3";
 import Hobby2 from "../../components/UI/Hobby/Hobby2";
@@ -9,25 +9,28 @@ import ball from "../../assets/images/ball.png";
 import laptop from "../../assets/images/laptop.png";
 
 class Hobbies extends Component {
-    render() {
-      return (
-        <div className={classes.Hobbies} id="Hobbies">
-            <HobbyInfo/>
-            <Hobby3/>
-            <div className={[classes.Img,classes.Img1].join(' ')}>
-              <img src={tabla} alt="tabla"/>
-            </div>
-            <div className={[classes.Img,classes.Img2].join(' ')}>
-              <img src={ball} alt="ball"/>
-            </div>
-            <div className={[classes.Img,classes.Img3].join(' ')}>
-              <img src={laptop} alt="cricket"/>
-            </div>
-            <Hobby2/>
-            <Hobby1/>
+  render() {
+    return (
+      <div className={classes.Hobbies} id="Hobbies">
+        <HobbyInfo />
+
+        <div className={classes.dispHobbies}>
+          <Hobby3 />
+          <Hobby2 />
+          <Hobby1 />
         </div>
-      );
-    }
+        <div className={[classes.Img, classes.Img1].join(" ")}>
+          <img src={tabla} alt="tabla" />
+        </div>
+        <div className={[classes.Img, classes.Img2].join(" ")}>
+          <img src={ball} alt="ball" />
+        </div>
+        <div className={[classes.Img, classes.Img3].join(" ")}>
+          <img src={laptop} alt="cricket" />
+        </div>
+      </div>
+    );
   }
-  
+}
+
 export default Hobbies;
